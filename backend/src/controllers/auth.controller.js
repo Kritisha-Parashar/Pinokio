d9485c47a45e5a69c4f470a8307c229d913619a5
Pavitra Mallick <pavitra.mallick2005@gmail.com>
2024-11-21 14:47:28 +0530

    Setup Tailwind

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js"
-import bcrypt from "bcrypt.js"
+import bcrypt from "bcryptjs"
 export const signup = async (req, res) => {
     const{fullName,email,password} = req.body
     try {
