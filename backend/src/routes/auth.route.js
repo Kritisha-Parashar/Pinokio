58998903485caf6a4037d5e44ec1d4bca37e8556
Kritisha <kritishaparashar123@gmail.com>
2024-11-21 14:42:49 +0530

    updated backend

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/auth.controller.js";
-
+import { protectRoute } from "../../middleware/auth.middleware.js";
 
 const router = express();
 
@@ -9,5 +9,9 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile",protectRoute, updateProfile);
+
+router.length("/check", protectRoute, checkAuth)
 
 export default router;
