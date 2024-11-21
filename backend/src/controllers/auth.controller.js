7db9ec46e8a03479082e0bc254c8dd6ff9a95610
Kritisha <kritishaparashar123@gmail.com>
2024-11-21 06:06:19 +0530

    final signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -10,7 +10,6 @@ export const signup = async (req, res) => {
         if(password.length < 6) {
             return res.status(400).json({ message: "Password must be at least 6 characters"});
         } 
-
         const user = await User.findOne({email})
 
         if (user) return res.status(400).json({ message: "Email already exists"});   
